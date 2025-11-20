c24607886b4c4284c57198722c2baf1002c3b967
mirkofoglia03-cloud <mirkofoglia03@gmail.com>
2025-11-20 22:24:58 +0100

    Update background image in HeroSection component

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -5,7 +5,7 @@ const HeroSection: React.FC = (): JSX.Element => {
     <section
       className="min-h-screen flex items-center justify-center pt-16 bg-cover bg-center relative"
       style={{
-        backgroundImage: 'url(https://drive.google.com/uc?export=view&id=1TmD1GbAdoYz3kggGrkacBUbpJ6RmySwx)',
+        backgroundImage: 'url(/images/hero-section.jpg)',
       }}
     >
       {/* Overlay scuro per leggibilità */}
